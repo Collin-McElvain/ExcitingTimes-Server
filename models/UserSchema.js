ad631ad81415e5bf93fc8e40779a2f500db16bef
Collin <cmcelvain@woodgrain.com>
2020-06-11 13:56:29 -0500

    Create user mongo schema

---
+++ models/UserSchema.js
@@ -0,0 +1,38 @@
+var mongoose = require("mongoose"),
+    bcrypt = require(bcrypt),
+    SALT_FACTOR = 7;
+
+var UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, index: {unique: true} },
+    password: { type: String, required: true }
+});
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    //Check for updated password
+    if(!user.isModified('password')) {
+        //return the callback function
+        return next();
+    }
+
+    //Salt password and hash with bcrypt (recommended method by mongoose)
+    bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+        if(err) {
+            return next(err);
+        }
+
+        //Hash with salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if(err) {
+                return next(err);
+            }
+
+            //Change password with reference to db object
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+var UserModel = mongoose.model('UserModel', UserSchema );
